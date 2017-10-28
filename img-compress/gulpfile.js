6fd05131cc3ebc0f8dac253ab1269fd78da126f5
backtonature <backtonature@foxmail.com>
2017-10-28 17:24:47 +0800

    update

--- img-compress/gulpfile.js
+++ img-compress/gulpfile.js
@@ -1,17 +1,20 @@
 const gulp = require('gulp');
-const cleanCSS = require('gulp-clean-css');
+
+const gulpif = require('gulp-if');
+const path = require('path');
+
+// 图片压缩
 const imagemin = require('gulp-imagemin');
 const imageminMozjpeg = require('imagemin-mozjpeg');
 const imageminPngquant = require('imagemin-pngquant');
 const imageminGifsicle = require('imagemin-gifsicle');
 const imageminSvgo = require('imagemin-svgo');
-const gulpif = require('gulp-if');
-const uglify = require('uglify-es');
-const composer = require('gulp-uglify/composer');
-const pump = require('pump');
-const path = require('path');
-const minify = composer(uglify, console);
 
+// js压缩混淆
+
+
+
+// css压缩
 
 var condition1 = function (file) {
     var fileExt = ['.jpg', '.png', '.gif', '.ico'];
@@ -20,25 +23,8 @@ var condition1 = function (file) {
     }
 };
 
-var condition2 = function (file) {
-    var fileExt = ['.js'];
-    if (fileExt.indexOf(path.extname(file.path)) >= 0) {
-        return true;
-    }
-};
-
-var condition3 = function (file) {
-    var fileExt = ['.css'];
-    if (fileExt.indexOf(path.extname(file.path)) >= 0) {
-        return true;
-    }
-};
-
-
 gulp.task('default', () =>
     gulp.src('src/**/*')
         .pipe(gulpif(condition1, imagemin([imageminGifsicle(), imageminMozjpeg(), imageminPngquant(), imageminSvgo()])))
-        .pipe(gulpif(condition2, minify({})))
-        .pipe(gulpif(condition3, cleanCSS({compatibility: 'ie8'})))
         .pipe(gulp.dest('dist'))
 );
