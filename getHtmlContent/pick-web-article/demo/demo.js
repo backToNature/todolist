7f131641b3fc6a9f475ab1d5573779584764c242
backtonature <backtonature@foxmail.com>
2017-10-30 19:24:00 +0800

    update

--- getHtmlContent/pick-web-article/demo/demo.js
+++ getHtmlContent/pick-web-article/demo/demo.js
@@ -1,59 +1,69 @@
-var http = require('http'), httpProxy = require('http-proxy');
-var querystring = require('querystring');
-var {URL} = require('url');
+const Koa = require('koa');
+const Router = require('koa-router');
+const request = require('request');
+let app = new Koa();
+const fs = require('fs');
+const {URL} = require('url');
+const url = require('url');
+const path = require('path');
+const iconv = require('iconv-lite');
+let router = new Router();
+var Buffer = require('buffer').Buffer;
 
-var proxy = httpProxy.createProxyServer({});  
-  
-// 捕获异常  
-proxy.on('error', function (err, req, res) {  
-  res.writeHead(500, {  
-    'Content-Type': 'text/plain'  
-  });  
-  res.end('Something went wrong. And we are reporting a custom error message.');  
-});  
-  
-// 另外新建一个 HTTP 80 端口的服务器，也就是常规 Node 创建 HTTP 服务器的方法。  
-// 在每次请求中，调用 proxy.web(req, res config) 方法进行请求分发  
-var server = require('http').createServer(function(req, res) {
-  var host = req.headers.host, ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;  
-  console.log("client ip:" + ip + ", host:" + host);
-  if (req.url.indexOf('web') >= 0) {
-      var _url = req.url.substring(req.url.indexOf('?') + 1, req.url.length);
-      _url = querystring.parse(_url).url;
-      var urlObj = new URL(_url);
-      _url = urlObj.protocol + '//' + urlObj.host;
-  }
-  if (_url) {
-      console.log(423423432);
-      proxy.web(req, res, { target: 'http://127.0.0.1' });
-      return;
-  } else {
-      proxy.web(req, res, { target: 'http://xyd.bbbbb.com:82' });
-  }
+let domain;
 
-  switch(host){
-    case 'aaaa.com':
-    case 'bbs.aaaa.com':
-        proxy.web(req, res, { target: 'http://xyd.bbbbb.com:82' });
-    break;
-    case 'vps.cccc.com':
-        proxy.web(req, res, { target: 'http://xyd.bbbbb.com:8080' });
-    break;
-    case 'dddd.com':
-    case 'www.dddd.com':
-        proxy.web(req, res, { target: 'http://localhost:81' });
-    break;
-    case 'eeeeee.com.cn':
-    case 'www.eeee.com.cn':
-        proxy.web(req, res, { target: 'http://eeeee.com.cn:8082' });
-    break;
-    default:
-        res.writeHead(200, {
-            'Content-Type': 'text/plain'
+
+let _doRequest = (url) => {
+    return new Promise((resolve, reject) => {
+        request(url, (err, res, body) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve({
+                    headers: res.headers,
+                    content: body
+                });
+            }
         });
-        res.end('Welcome to my server!');
-  }
-});  
-  
-console.log("listening on port 80");
-server.listen(80);  
+    });
+};
+
+router.get('/qb_pick_test', async (ctx, next) => {
+    let content = fs.readFileSync(path.join(__dirname, './static/demo.html'), 'utf8');
+    ctx.body = content;
+});
+
+router.get('/qb_pick_test/web', async (ctx, next) => {
+    let _url = ctx.query.url;
+    let urlObj = new URL(_url);
+    domain = urlObj.protocol + '//' + urlObj.host;
+    let obj = await _doRequest(_url);
+    let type = obj.headers['content-type'];
+    if (type) {
+        if (type.toUpperCase().indexOf('GB2312') >= 0) {
+            // console.log('啊啊', iconv.decode('啊啊', 'gb2312'));
+            // console.log(iconv.decode(obj.content, 'gb2312').toString());
+            obj.content = iconv.decode(obj.content, 'gb2312').toString();
+        } else if (type.toUpperCase().indexOf('GBK') >= 0) {
+            obj.content = iconv.decode(obj.content, 'gbk');
+        }
+    }
+    ctx.set(obj.headers);
+    ctx.body = obj.content;
+});
+
+router.get('/*', async (ctx, next) => {
+    let trueUrl = url.resolve(domain, ctx.path);
+    console.log(trueUrl);
+    let content = await request(trueUrl);
+    ctx.body = content;
+    // console.log(url.resolve(domain, ctx.url));
+    // console.log(domain, ctx.url);
+});
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+app.listen(3000, () => {
+    console.log('server listening on 3000');
+});
