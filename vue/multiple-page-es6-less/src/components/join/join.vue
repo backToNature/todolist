5067bcd7a9f79d7bcff99f51406707f6314eaf0c
chaoyidaozhan <2323791346@qq.com>
2017-11-10 22:45:16 +0800

    修改join样式

--- vue/multiple-page-es6-less/src/components/join/join.vue
+++ vue/multiple-page-es6-less/src/components/join/join.vue
@@ -5,9 +5,9 @@
           <img src="../../static/join_top.jpg" alt="" class="join-pic">
         </div>
         <div class="join-tech">
-            <div class="global-tech-title">
-                <p class="global-tech-title-a">T E C H N O L O G Y</p>
-                <p class="global-tech-title-b">校 园 招 聘</p>
+            <div class="join-tech-title">
+                <p class="join-tech-title-a">T E C H N O L O G Y</p>
+                <p class="join-tech-title-b">校 园 招 聘</p>
             </div>
             <div class="join-tech-list">
               <div class="join-tech-content" v-for="(item,index) in tech">
@@ -15,6 +15,7 @@
                     <div class="join-line" v-if="index != 0">
                       <div class="join-line-img"></div>
                     </div>
+                 
                     <div class="tech-title">
                       <div class="tech-title-img"></div>
                       <div class="tech-title-word2">
@@ -23,7 +24,7 @@
                     </div>
                     <p class="tect-example">岗位职责</p>
                     <p class="tect-example2" v-html="item.response"></p>
-                    <p class="tect-example">任职要求</p>
+                    <p class="tect-example3">任职要求</p>
                     <p class="tect-example2" v-html="item.require"></p>
                   </div>
               </div>
@@ -42,12 +43,12 @@
                   {
                     title:'资深视觉工程师',
                     response:'研发高质量计算机视觉算法和产品',
-                    require:'有扎实的图像分析埃里克多饥渴发酵素看的就发卡量讲的放抗裂砂浆啊妖孽啊哦IE欧萨大姐夫卡拉胶啥都我监控橘黄色的<br/><br/>按付款链接哦我金'
+                    require:'有扎实的图像分析埃里克多饥渴发酵素看的就发卡量讲的放抗裂砂浆啊妖孽啊哦IE欧萨大姐夫卡拉胶啥都我监控橘黄色的<br/><br/>按付款链接哦我金刚萨埵科技感俊美的惊愕ID抗裂砂浆'
                   },
                   {
                     title:'资深嵌入式软件工程师',
                     response:'研发高质量计算机视觉算法和产品',
-                    require:'有扎实的图像分析埃里克多饥渴发酵素看的就发卡量讲的放抗裂砂浆啊妖孽啊哦IE欧萨大姐夫卡拉胶啥都我监控橘黄色的<br/><br/>按付款链接哦我金'
+                    require:'有扎实的图像分析埃里克多饥渴发酵素看的就发卡量讲的放抗裂砂浆啊妖孽啊哦IE欧萨大姐夫卡拉胶啥都我监控橘黄色的<br/><br/>按付款链接哦我金刚萨埵科技感俊美的惊愕ID抗裂砂浆'
                   }
                 ]
             }
