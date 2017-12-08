330b7bace87db31fbda3152e7e8e8085ea513602
backToNature <backtonature@foxmail.com>
2017-12-08 09:16:28 +0800

    update

--- headless-chrome/index.js
+++ headless-chrome/index.js
@@ -1,26 +1,28 @@
 const CDP = require("chrome-remote-interface");
-
-console.log(123123);
+// const chromeLauncher = require('chrome-launcher')
 CDP(client => {
- // extract domains
- const { Network, Page } = client;
- // setup handlers
- Network.requestWillBeSent(params => {
-   console.log(params.request.url);
- });
- Page.loadEventFired(() => {
-   client.close();
- });
- // enable events then start!
- Promise.all([Network.enable(), Page.enable()])
-   .then(() => {
-     return Page.navigate({ url: "https://github.com" });
-   })
-   .catch(err => {
-     console.error(err);
+    // extract domains
+    const { Network, Page } = client;
+    // setup handlers
+    Network.requestWillBeSent(params => {
+     console.log(params.request.url);
+    });
+    Page.loadEventFired(() => {
      client.close();
-   });
-}).on("error", err => {
- // cannot connect to the remote endpoint
- console.error(err);
+    });
+    // enable events then start!
+    Promise.all([Network.enable(), Page.enable()])
+     .then(() => {
+       return Page.navigate({ url: "http://http://www.sohu.com" });
+     })
+     .catch(err => {
+       console.error(err);
+       client.close();
+     });
+    }).on("error", err => {
+    // cannot connect to the remote endpoint
+    console.error(err);
 });
+
+
+
--- headless-chrome/test.js
+++ headless-chrome/test.js
@@ -1,2 +1,11 @@
-const {ChromeLauncher} = require('lighthouse/lighthouse-cli/chrome-launcher');
-console.log(123);
+const chromeLauncher = require('chrome-launcher')
+// const chrome = require('chrome-remote-interface')
+// const fs = require('fs')
+
+chromeLauncher.launch({
+  startingUrl: 'http://www.qq.com',
+  port: 9222,
+  chromeFlags: ['--headless', '--disable-gpu']
+}).then(chrome => {
+  console.log(`Chrome debugging port running on ${chrome.port}`);
+});
