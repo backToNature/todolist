3dcfea27621a868c3f1335f465223518def6c883
daringuo(郭豪) <daringuo@tencent.com>
2018-01-17 22:24:29 +0800

    update

--- fe-data-log/index.js
+++ fe-data-log/index.js
@@ -2,8 +2,9 @@ let timing = performance.timing;
 
 let feLogger = {
     performance: {
+        dns: timing.domainLookupEnd - timing.domainLookupStart, // dns查询时间
         whiteScreen: timing.responseStart - timing.navigationStart, // 白屏时间
-        
-
+        domready: timing.domContentLoadedEventEnd - timing.navigationStart, // domready
+        onload: timing.loadEventEnd - timing.navigationStart
     }
 };
