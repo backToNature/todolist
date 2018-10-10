bfd8a55354e6fdd2164085cef66dd4e75935ed68
backtonature <backtonature@foxmail.com>
2018-10-10 19:59:38 +0800

    update

---
+++ vue/vue-example/ssr/server/public/component/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="index">
+    <h1>郭豪{{user}}</h1>
+  </div>
+</template>
+<style>
+  h1 {
+    color: red;
+  }
+</style>
+<script>
+export default {
+  data() {
+    return {
+      user: 'guohao'
+    }
+  }
+}
+</script>
+
